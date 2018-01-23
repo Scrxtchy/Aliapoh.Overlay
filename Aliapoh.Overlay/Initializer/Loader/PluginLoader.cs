6e8bea296447a03d3c644c45513a7082acd94811
Laighlinne <mylastmogletter@gmail.com>
2018-01-23 14:56:18 +0900

    snapshot

---
+++ Aliapoh.Overlay/Initializer/Loader/PluginLoader.cs
@@ -0,0 +1,19 @@
+﻿using Aliapoh.Overlay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliapoh.Initializer
+{
+    public class PluginLoader
+    {
+        private AssemblyResolver ASMResolver;
+
+        public PluginLoader()
+        {
+
+        }
+    }
+}
